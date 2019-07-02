a4044f2a8c2df841b84e219554aa34f0bbb2f15c
Nomahd <cichawk@gmail.com>
2019-07-03 02:13:31 +0900

    Version 0.3
    Fixing migrations

--- db/migrate/20190702170702_make_messengers_unique.rb
+++ db/migrate/20190702170702_make_messengers_unique.rb
@@ -1,5 +1,5 @@
 class MakeMessengersUnique < ActiveRecord::Migration[5.2]
   def change
-    add_index :messengers, :name, unique: true, name: 'msg'
+    add_index :messengers, :name, unique: true
   end
 end
