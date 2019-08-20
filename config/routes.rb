e92d1e8eb6a96ecab1be85aca18310707e585daa
Nomahd <cichawk@gmail.com>
2019-08-20 17:16:59 +0900

    Adding API Controller

--- config/routes.rb
+++ config/routes.rb
@@ -18,8 +18,9 @@ Rails.application.routes.draw do
     if Rails.env.development?
       get 'view_tester/test'
     end
-
   end
+
+  get '/api/:today', to: 'api#today'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
