8e12e2fa996aac74969d2465b1af6b76816353ec
Nomahd <cichawk@gmail.com>
2019-08-20 18:01:41 +0900

    Changed API response from array to pure object

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,8 +1,8 @@
 class ApiController < ApplicationController
 
   def today
-    render json: [ :devotion => Devotion.select(:id, :title, :broadcast_date).order(:broadcast_date).last,
+    render json: { :devotion => Devotion.select(:id, :title, :broadcast_date).order(:broadcast_date).last,
                     :audio => Audio.select(:id, :title, :broadcast_date).order(:broadcast_date).last,
-                    :video => Video.select(:id, :title, :broadcast_date).order(:broadcast_date).last]
+                    :video => Video.select(:id, :title, :broadcast_date).order(:broadcast_date).last }
   end
 end
