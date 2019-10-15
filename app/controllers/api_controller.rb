a7397cdb209590cf4ea114acac71f431dbdfe6fb
Nomahd <cichawk@gmail.com>
2019-10-15 23:19:11 +0900

    Fix and improve API search

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -51,67 +51,122 @@ class ApiController < ApplicationController
 
   def search
     model = Object.const_get(params[:model])
-    search = []
-    db_model_name = params[:model].downcase + 's'
+    search_results = []
+    db_model_name = params[:model].downcase.pluralize
+
+    # Checking text
     unless params[:text].blank?
-      tags = []
       text = params[:text].split
+
+      # Search tags
       text.each do |t|
         tagged = model.tagged_with(t).as_json
-        unless tagged[0].nil?
-          tagged[0].slice!('id', 'title', 'broadcast_date')
-          fix_tagged = {:id=>tagged[0]["id"], :title=>tagged[0]["title"], :broadcast_date=>tagged[0]["broadcast_date"]}
-          tags.push(fix_tagged)
+        unless tagged.blank?
+          tagged.each do |tag|
+            tag.slice!('id', 'title', 'broadcast_date')
+            tag_result = {:id => tag['id'], :title=> tag['title'], :broadcast_date=> tag['broadcast_date']}
+            search_results |= [tag_result]
+            puts "search tags"
+            puts search_results.inspect
+          end
         end
       end
 
-      array = []
-      baseQuery = "SELECT  `" + db_model_name + "`.`id`, `" + db_model_name + "`.`title`, `" + db_model_name + "`.`broadcast_date` FROM `" + db_model_name + "` WHERE "
-      query = baseQuery
+      # Search title
+      query_base_title = "SELECT  `" + db_model_name + "`.`id`, `" + db_model_name + "`.`title`, `" + db_model_name + "`.`broadcast_date` FROM `" + db_model_name + "` WHERE "
+      query_title = query_base_title
       text.each_with_index do |t, i|
         unless i == 0
-          query += " OR "
+          query_title += " OR "
         end
-        query += "title LIKE '%" + t + "%'"
+        query_title += "title LIKE '%" + t + "%'"
       end
-      result = ActiveRecord::Base.connection.execute(query)
-      result.each do |d|
-        array.push({:id => d[0], :title => d[1], :broadcast_date => d[2]})
+      title_results = ActiveRecord::Base.connection.execute(query_title)
+      title_results.each do |d|
+        puts d.inspect
+        title_result_hash = {:id => d[0], :title => d[1], :broadcast_date => d[2]}
+        search_results |= [title_result_hash]
+        puts "search titles"
+        puts search_results.inspect
       end
-      search = array + tags
+
+      # Search description for Audio and Video
+      if model == Audio or model == Video
+        query_base_description = "SELECT  `" + db_model_name + "`.`id`, `" + db_model_name + "`.`title`, `" + db_model_name + "`.`broadcast_date` FROM `" + db_model_name + "` WHERE "
+        query_description = query_base_description
+        text.each_with_index do |t, i|
+          unless i == 0
+            query_description += " OR "
+          end
+          query_description += "description LIKE '%" + t + "%'"
+        end
+        description_results = ActiveRecord::Base.connection.execute(query_description)
+        description_results.each do |d|
+          description_results_hash = {:id => d[0], :title => d[1], :broadcast_date => d[2]}
+          search_results |= [description_results_hash]
+          puts "search descriptions"
+          puts search_results.inspect
+        end
+
+        #Search body for Devotion
+      elsif model == Devotion
+        body_search_array = []
+        query_base_body = "SELECT  `" + db_model_name + "`.`id`, `" + db_model_name + "`.`title`, `" + db_model_name + "`.`broadcast_date` FROM `" + db_model_name + "` WHERE "
+        query_body = query_base_body
+        text.each_with_index do |t, i|
+          unless i == 0
+            query_body += " OR "
+          end
+          query_body += "body LIKE '%" + t + "%'"
+        end
+        title_results = ActiveRecord::Base.connection.execute(query_body)
+        title_results.each do |d|
+          body_search_array.push({:id => d[0], :title => d[1], :broadcast_date => d[2]})
+        end
+        search_results |= body_search_array
+        puts "search body"
+        puts search_results.inspect
+      end
     end
 
+    # Search date
     unless params[:year].blank? and params[:month].blank?
-      array = []
-      baseQuery = "SELECT  `" + db_model_name + "`.`id`, `" + db_model_name + "`.`title`, `" + db_model_name + "`.`broadcast_date` FROM `" + db_model_name + "`"
+      date_search_array = []
+      query_base_date = "SELECT  `" + db_model_name + "`.`id`, `" + db_model_name + "`.`title`, `" + db_model_name + "`.`broadcast_date` FROM `" + db_model_name + "`"
 
-      query = baseQuery
+      query_date = query_base_date
       unless params[:month].blank?
         monthQuery = " WHERE (extract(month from broadcast_date) = '" + params[:month] + "')"
-        query += monthQuery
+        query_date += monthQuery
       end
 
       unless params[:year].blank?
         if params[:month].blank?
-          query += " WHERE "
+          query_date += " WHERE "
         else
-          query += " AND "
+          query_date += " AND "
         end
         yearQuery = " (extract(year from broadcast_date) = '" + params[:year] + "')"
-        query += yearQuery
+        query_date += yearQuery
       end
 
-      result = ActiveRecord::Base.connection.execute(query)
-      result.each do |d|
-        array.push({:id => d[0], :title => d[1], :broadcast_date => d[2]})
+      title_results = ActiveRecord::Base.connection.execute(query_date)
+      title_results.each do |d|
+        date_search_array.push({:id => d[0], :title => d[1], :broadcast_date => d[2]})
       end
-      if search.empty?
-        search = array
+      if search_results.empty?
+        search_results = date_search_array
+        puts "search only dates"
+        puts search_results.inspect
       else
-        search = search & array
+        search_results = search_results & date_search_array
+        puts "search dates"
+        puts search_results.inspect
       end
     end
-    sorted = search.sort_by {|obj| obj[:broadcast_date]}.reverse
-    render json: sorted.take(50)
+    puts "final result"
+    puts search_results.inspect
+    sorted_search_results = search_results.sort_by {|obj| obj[:broadcast_date]}.reverse
+    render json: sorted_search_results.take(50)
   end
 end
